568079b1d70d4823660f409e9f84bbc2f7be6ac7
Anton Bravin <baboonweb@gmail.com>
2018-08-15 13:57:43 +0400

    Lerps rework. Remove duplacte class

--- Assets/CoreToolkit/Lerp/CoreToolkitLerpManager.cs
+++ Assets/CoreToolkit/Lerp/CoreToolkitLerpManager.cs
@@ -8,23 +8,16 @@ namespace CoreToolkit {
 
         private List<CoreToolkitLerp> _tasksList = new List<CoreToolkitLerp>();
 
-        private List<CoreToolkitLerp> _toRemoveTaskList = new List<CoreToolkitLerp>();
-
         public void AddLerpTask(CoreToolkitLerp lerpParams) {
             _tasksList.Add(lerpParams);
         }
 
         public void RemoveLerpTask(CoreToolkitLerp lerpParams) {
-            _toRemoveTaskList.Add(lerpParams);
+            _tasksList.Remove(lerpParams);
         }
 
         void Update() {
-            foreach(CoreToolkitLerp task in _toRemoveTaskList) {
-                _tasksList.Remove(task);
-            }
-            _toRemoveTaskList.Clear();
-
-            foreach(CoreToolkitLerp task in _tasksList) {
+            foreach(CoreToolkitLerp task in _tasksList.ToArray()) {
                 task.Iterate();
             }
         }
