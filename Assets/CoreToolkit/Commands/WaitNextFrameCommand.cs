11981f0df72af33f20466ed87abca0d3e955a5a7
Anton Bravin <baboonweb@gmail.com>
2019-07-29 12:47:29 +0400

    Remove LerpVector3. Add WaitNextFrameCommand. Clean deps.

---
+++ Assets/CoreToolkit/Commands/WaitNextFrameCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoreToolkit.Command {
+
+    public class WaitNextFrameCommand : BaseCommand {
+        private Coroutine _delayCoroutine;
+
+        protected override void ExecInternal() {
+            _delayCoroutine = CoreToolkitCommandMB.Instance().StartCoroutine(Delay());
+        }
+
+        private IEnumerator Delay() {
+            yield return new WaitForEndOfFrame();
+            NotifyComplete();
+        }
+    }
+}
--- Assets/CoreToolkit/Lerp/CoreToolkitLerp.cs
+++ Assets/CoreToolkit/Lerp/CoreToolkitLerp.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 namespace CoreToolkit {
 
     public class CoreToolkitLerp {
-
         public float From;
         public float To;
-        public Vector3 FromVector;
-        public Vector3 ToVector;
         public float Time;
 
         private float _progress;
@@ -17,9 +14,6 @@ namespace CoreToolkit {
 
         private float _value;
         public float Value { get { return _value; } }
-
-        private Vector3 _valueVector;
-        public Vector3 ValueVector { get {return _valueVector;} }
 
         private float _startTime;
 
@@ -35,34 +29,21 @@ namespace CoreToolkit {
 
             _startTime = UnityEngine.Time.time;
             CoreToolkitLerpManager.Instance().AddLerpTask(this);
-        }
-        
-        public CoreToolkitLerp(Vector3 from, Vector3 to, float time) {
-            FromVector = from;
-            ToVector = to;
-            Time = time;
-            
-            _startTime = UnityEngine.Time.time;
-            CoreToolkitLerpManager.Instance().AddLerpTask(this);
+
         }
 
         public void Iterate() {
             _progress = Mathf.Clamp01((UnityEngine.Time.time - _startTime) / Time);
             _value = Mathf.Lerp(From, To, _progress);
-            _valueVector = Vector3.Lerp(FromVector, ToVector, _progress);
 
             if (OnLerpUpdate != null)
                 OnLerpUpdate(this);
 
-            if (_progress == 1f)
-                Terminate();
-        }
-        
-        public void Terminate() {
-            if(OnLerpEnd != null)
+            if (_progress == 1f && OnLerpEnd != null) {
                 OnLerpEnd(this);
-            
-            CoreToolkitLerpManager.Instance().RemoveLerpTask(this);
+                CoreToolkitLerpManager.Instance().RemoveLerpTask(this);
+            }
+                
         }
 
     }
