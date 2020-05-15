177024223ee778c6ec73acf120685b3f5b6afdc6
Anton Bravin <baboonweb@gmail.com>
2020-05-15 19:17:10 +0400

    0.2.0 Fix import as package. Add Fader

---
+++ Assets/Runtime/Commands/ParallelCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CoreToolkit.Command {
+
+    public class ParallelCommand : BaseCommand {
+        
+        public List<ICommand> pool = new List<ICommand>();
+        protected event Action<ParallelCommand, ICommand> CommandCompleteEvent;
+        
+        public void Add(ICommand command) {
+            pool.Add(command);
+            Complete = false;
+        }
+        
+        protected override void ExecInternal() {
+            foreach(ICommand cmd in pool) {
+                cmd.AddCompleteHandler(OnCommandComplete);
+                cmd.Execute();
+            }
+        }
+
+        protected virtual void OnCommandComplete(ICommand cmd) {
+            if (CommandCompleteEvent != null)
+                CommandCompleteEvent(this, cmd);
+
+            pool.Remove(cmd);
+            if (pool.Count == 0)
+                NotifyComplete();
+        }
+
+        protected override void NotifyComplete() {
+            base.NotifyComplete();
+            CommandCompleteEvent = null;
+        }
+    }
+}
---
+++ Assets/Runtime/CoreToolkitFader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CoreToolkit.Fader {
+    public class CoreToolkitFader : MBSingleton<CoreToolkitFader> {
+
+        public Action OnFadeComplete;
+
+        private Image _faderImage;
+        private CoreToolkitLerp _fadeLerp;
+
+        private void Awake() {
+            if (_faderImage == null)
+                LoadFader();
+        }
+
+        private void LoadFader() {
+            GameObject faderObjectPrefab = Resources.Load<GameObject>("CoreToolkitFaderCanvas");
+            if (faderObjectPrefab == null)
+                return;
+
+            GameObject _faderObject = Instantiate(faderObjectPrefab, transform);
+            _faderObject.name = "CoreToolkitFaderCanvas";
+
+            _faderImage = _faderObject.GetComponentInChildren<Image>();
+            _faderImage.color = new Color(1f, 1f, 1f, 0f);
+            _faderImage.raycastTarget = false;
+        }
+
+        public void Fade(FadeConfig fadeConfig) {
+            _fadeLerp?.Stop();
+
+            _faderImage.raycastTarget = fadeConfig.IsLockedRaycasts;
+            _faderImage.color = new Color(fadeConfig.FaderColor.r,
+                                          fadeConfig.FaderColor.g,
+                                          fadeConfig.FaderColor.b,
+                                          fadeConfig.FromFaderAlpha);
+
+            _fadeLerp = new CoreToolkitLerp(fadeConfig.FromFaderAlpha,
+                                                       fadeConfig.ToFaderAlpha,
+                                                       fadeConfig.Time);
+
+            _fadeLerp.OnLerpUpdate += (lrp) => {
+                Color currentColor = _faderImage.color;
+                currentColor.a = fadeConfig.FadeFunction(lrp.Value);
+                _faderImage.color = currentColor;
+            };
+
+            _fadeLerp.OnLerpEnd += (lrp) => {
+                _faderImage.raycastTarget = false;
+                OnFadeComplete?.Invoke();
+            };
+        }
+
+    }
+
+    public class FadeConfig {
+        public float FromFaderAlpha;
+        public float ToFaderAlpha;
+        public float Time;
+        public Color FaderColor = Color.black;
+        public bool IsLockedRaycasts = true;
+
+        public delegate float FadeFuncDelegate(float input);
+        public FadeFuncDelegate FadeFunction = (float input) => { return input; };
+
+        public FadeConfig(float from, float to, float time) {
+            FromFaderAlpha = from;
+            ToFaderAlpha = to;
+            Time = time;
+        }
+    }
+}
