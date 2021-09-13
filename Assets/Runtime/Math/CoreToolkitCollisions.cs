c946cb4913aae83059aacec27e13fa08dd547552
Anton Bravin <baboonweb@gmail.com>
2021-09-13 08:11:14 +0400

    Add CircleRectangle overlap function

---
+++ Assets/Runtime/Math/CoreToolkitCollisions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+namespace CoreToolkit.Math.Collisions {
+
+    public class CoreToolkitCollisions {
+
+        public static bool CircleRectangle(Vector2 circlePos, float circleRadius, Rect rect) {
+            Vector2 closestPoint = circlePos;
+
+            closestPoint.x = (closestPoint.x < rect.min.x) ? rect.min.x : closestPoint.x;
+            closestPoint.x = (closestPoint.x > rect.max.x) ? rect.max.x : closestPoint.x;
+
+            closestPoint.y = (closestPoint.y < rect.min.y) ? rect.min.y : closestPoint.y;
+            closestPoint.y = (closestPoint.y > rect.max.y) ? rect.max.y : closestPoint.y;
+
+            Vector2 offset = circlePos - closestPoint;
+            return offset.sqrMagnitude < circleRadius * circleRadius;
+        }
+
+    }
+
+}
