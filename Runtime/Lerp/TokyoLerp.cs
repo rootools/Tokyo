9754da0d1bee5aa7ef173fa4b982311ab6c57a5a
Anton Bravin <baboonweb@gmail.com>
2022-03-25 11:04:21 +0400

    TokyoLerp: Add new Constructor with 0-1 default. Add ability to set progress manualy. Cleanup some properties

--- Runtime/Lerp/TokyoLerp.cs
+++ Runtime/Lerp/TokyoLerp.cs
@@ -4,28 +4,31 @@ using Tokyo.Math;
 namespace Tokyo {
 
     public class TokyoLerp {
-        public float From;
-        public float To;
-        public float Time;
-        public EaseType Easing = EaseType.Linear;
-
-        public float Progress { get; private set; }
-        public float Value { get; private set; }
-
-        private float _startTime;
-
-        private float _startPauseTime;
-        private float _pauseTimeOffset;
-
-        private bool _isIgnoreTimeScale;
-
-        private float CurrentTime => (_isIgnoreTimeScale) ? UnityEngine.Time.realtimeSinceStartup : UnityEngine.Time.time;
-
-        public bool IsPaused { get; private set; }
 
         public delegate void OnLerpUpdateDelegate(TokyoLerp lerp);
         public event OnLerpUpdateDelegate OnLerpUpdate;
         public event OnLerpUpdateDelegate OnLerpEnd;
+
+        public float From { get; private set; }
+        public float To { get; private set; }
+        public float Time { get; private set; }
+        public EaseType Easing  { get; private set; }
+
+        public float Progress { get; private set; }
+        public float Value { get; private set; }
+        public bool IsPaused { get; private set; }
+        public bool IsStopped { get; private set; }
+
+        private float CurrentTime => (_isIgnoreTimeScale) ? UnityEngine.Time.realtimeSinceStartup : UnityEngine.Time.time;
+
+        private readonly float _startTime;
+        private readonly bool _isIgnoreTimeScale;
+
+        private float _startPauseTime;
+        private float _pauseTimeOffset;
+        private float _manualSetProgress;
+
+        public TokyoLerp(float time, EaseType easeType = EaseType.Linear, bool isIgnoreTimeScale = false) : this(0f, 1f, time, easeType, isIgnoreTimeScale) {}
 
         public TokyoLerp(float from, float to, float time, EaseType easeType = EaseType.Linear, bool isIgnoreTimeScale = false) {
             From = from;
@@ -43,14 +46,22 @@ namespace Tokyo {
             if (IsPaused)
                 return;
 
-            Progress = (Time == 0f) ? 1f: Mathf.Clamp01((CurrentTime - _pauseTimeOffset - _startTime) / Time);
+            if (Time == 0f)
+                Progress = 1f;
+            else {
+                float passedTime = CurrentTime - _pauseTimeOffset - _startTime;
+                float requiredTime = Time - (Time * _manualSetProgress);
+
+                Progress = (requiredTime == 0f) ? 1f : Mathf.Clamp01(passedTime / requiredTime);
+            }
+
             Value = Mathf.Lerp(From, To, TokyoEasings.Ease(Progress, Easing));
 
             OnLerpUpdate?.Invoke(this);
 
             if (Progress == 1f) {
                 OnLerpEnd?.Invoke(this);
-                TokyoLerpManager.Instance().RemoveLerpTask(this);
+                Stop();
             }
         }
 
@@ -58,6 +69,7 @@ namespace Tokyo {
             OnLerpUpdate = null;
             OnLerpEnd = null;
             TokyoLerpManager.Instance().RemoveLerpTask(this);
+            IsStopped = true;
         }
 
         public void Pause() {
@@ -68,6 +80,10 @@ namespace Tokyo {
         public void Resume() {
             IsPaused = false;
             _pauseTimeOffset = CurrentTime - _startPauseTime;
+        }
+
+        public void SetProgress(float progress) {
+            _manualSetProgress = progress;
         }
 
     }
