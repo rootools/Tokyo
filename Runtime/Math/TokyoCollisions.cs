0bdc3184162ec3fcea3d6790bcfd17fe3a9b02a5
Anton Bravin <baboonweb@gmail.com>
2024-01-11 09:31:57 +0400

    Optimization: TokyCollision.IntersectLines2D for best performance with Vector2 & Vector3

--- Runtime/Math/TokyoCollisions.cs
+++ Runtime/Math/TokyoCollisions.cs
@@ -7,7 +7,7 @@ namespace Tokyo.Math.Collisions {
         public static bool CircleRectangle(Vector2 circlePos, float circleRadius, Rect rect) {
             return CircleRectangle(circlePos, circleRadius, rect.min, rect.max);
         }
-        
+
         public static bool CircleRectangle(Vector2 circlePos, float circleRadius, Bounds bounds) {
             return CircleRectangle(circlePos, circleRadius, bounds.min, bounds.max);
         }
@@ -26,10 +26,10 @@ namespace Tokyo.Math.Collisions {
 
             _circleRectangleClosestPoint.x = circlePos.x - _circleRectangleClosestPoint.x;
             _circleRectangleClosestPoint.y = circlePos.y - _circleRectangleClosestPoint.y;
-            
+
             return _circleRectangleClosestPoint.sqrMagnitude < circleRadius * circleRadius;
         }
-        
+
         public static bool IntersectLinesPoint(Vector2 l1pa, Vector2 l1pb, Vector2 l2pa, Vector2 l2pb, ref Vector2 intersectPoint) {
             float denominator = ((l1pb.x - l1pa.x) * (l2pb.y - l2pa.y)) - ((l1pb.y - l1pa.y) * (l2pb.x - l2pa.x));
 
@@ -38,7 +38,7 @@ namespace Tokyo.Math.Collisions {
 
             float numerator1 = ((l1pa.y - l2pa.y) * (l2pb.x - l2pa.x)) - ((l1pa.x - l2pa.x) * (l2pb.y - l2pa.y));
             float numerator2 = ((l1pa.y - l2pa.y) * (l1pb.x - l1pa.x)) - ((l1pa.x - l2pa.x) * (l1pb.y - l1pa.y));
-    
+
             if (numerator1 == 0 || numerator2 == 0) // lines are coincident and intersect everywhere
                 return false;
 
@@ -52,22 +52,30 @@ namespace Tokyo.Math.Collisions {
 
                 intersectPoint.x = x;
                 intersectPoint.y = y;
-        
+
                 return true;
             }
-            
+
             return false;
         }
-        
-        public static bool IntersectLines(Vector2 l1pa, Vector2 l1pb, Vector2 l2pa, Vector2 l2pb) {
-            float denominator = ((l1pb.x - l1pa.x) * (l2pb.y - l2pa.y)) - ((l1pb.y - l1pa.y) * (l2pb.x - l2pa.x));
+
+        public static bool IntersectLines2D(Vector3 l1pa, Vector3 l1pb, Vector3 l2pa, Vector3 l2pb) {
+            return IntersectLines2D(l1pa.x, l1pa.y, l1pb.x, l1pb.y, l2pa.x, l2pa.y, l2pb.x, l2pb.y);
+        }
+
+        public static bool IntersectLines2D(Vector2 l1pa, Vector2 l1pb, Vector2 l2pa, Vector2 l2pb) {
+            return IntersectLines2D(l1pa.x, l1pa.y, l1pb.x, l1pb.y, l2pa.x, l2pa.y, l2pb.x, l2pb.y);
+        }
+
+        public static bool IntersectLines2D(float l1pax, float l1pay, float l1pbx, float l1pby, float l2pax, float l2pay, float l2pbx, float l2pby) {
+            float denominator = ((l1pbx - l1pax) * (l2pby - l2pay)) - ((l1pby - l1pay) * (l2pbx - l2pax));
 
             if (denominator == 0) // lines are parallel and will never intersect
                 return false;
 
-            float numerator1 = ((l1pa.y - l2pa.y) * (l2pb.x - l2pa.x)) - ((l1pa.x - l2pa.x) * (l2pb.y - l2pa.y));
-            float numerator2 = ((l1pa.y - l2pa.y) * (l1pb.x - l1pa.x)) - ((l1pa.x - l2pa.x) * (l1pb.y - l1pa.y));
-    
+            float numerator1 = ((l1pay - l2pay) * (l2pbx - l2pax)) - ((l1pax - l2pax) * (l2pby - l2pay));
+            float numerator2 = ((l1pay - l2pay) * (l1pbx - l1pax)) - ((l1pax - l2pax) * (l1pby - l1pay));
+
             if (numerator1 == 0 || numerator2 == 0) // lines are coincident and intersect everywhere
                 return false;
 
@@ -77,10 +85,9 @@ namespace Tokyo.Math.Collisions {
             // lines intersect each other
             if ((r >= 0 && r <= 1) && (s >= 0 && s <= 1))
                 return true;
-            
+
             return false;
         }
-
     }
 
 }
